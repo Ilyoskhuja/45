4e5b39d72ecf235ecdf4e191b9fc121be4391824
Ilyoskhuja <mr.ikromhodjayev@gmail.com>
2021-11-27 14:21:19 +0500

    refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -932,12 +932,10 @@ export class AppComponent {
             // this.treegrid.refresh();
             this.dataManager
               .executeQuery(new Query())
-              .then(
-                (e: ReturnOption) => (
-                  (this.data = e.result.data as object[]),
-                  this.treegrid.refresh()
-                )
-              )
+              .then((e: ReturnOption) => {
+                this.data = e.result.data as object[];
+                this.treegrid.refresh();
+              })
               .catch((e) => true);
           });
         this.http
@@ -992,12 +990,10 @@ export class AppComponent {
             console.log("post:------------------2", data);
             this.dataManager
               .executeQuery(new Query())
-              .then(
-                (e: ReturnOption) => (
-                  (this.data = e.result.data as object[]),
-                  this.treegrid.refresh()
-                )
-              )
+              .then((e: ReturnOption) => {
+                this.data = e.result.data as object[];
+                this.treegrid.refresh();
+              })
               .catch((e) => true);
           });
         this.dataManager
@@ -1053,12 +1049,10 @@ export class AppComponent {
               .subscribe((data) => {
                 this.dataManager
                   .executeQuery(new Query())
-                  .then(
-                    (e: ReturnOption) => (
-                      (this.data = e.result.data as object[]),
-                      this.treegrid.refresh()
-                    )
-                  )
+                  .then((e: ReturnOption) => {
+                    this.data = e.result.data as object[];
+                    this.treegrid.refresh();
+                  })
                   .catch((e) => true);
               });
           });
@@ -1101,12 +1095,10 @@ export class AppComponent {
             console.log("post:------------------4", data);
             this.dataManager
               .executeQuery(new Query())
-              .then(
-                (e: ReturnOption) => (
-                  (this.data = e.result.data as object[]),
-                  this.treegrid.refresh()
-                )
-              )
+              .then((e: ReturnOption) => {
+                this.data = e.result.data as object[];
+                this.treegrid.refresh();
+              })
               .catch((e) => true);
           });
 
